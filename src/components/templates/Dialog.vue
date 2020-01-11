8c13b2bd1ddf7b78826ec634b0393301eb0fdf9d
Nke0628 <ooo_diva@yahoo.co.jp>
2020-01-11 14:18:05 +0000

    ダイアログのテスト #1

--- src/components/templates/Dialog.vue
+++ src/components/templates/Dialog.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+  <div>
+    <ElButton @click="visible=!visible">起動</ElButton>
+    <ElDialog append-to-body :visible.sync="visible">
+      <template v-slot:footer>
+        <ElButton>保存</ElButton>
+      </template>
+    </ElDialog>
+  </div>
+</template>
 
-<script>
+<script lang="ts">
 import { Component, Prop, Emit, PropSync, Vue } from "vue-property-decorator";
 
 @Component
-export default class Dialog extends Vue {}
+export default class Dialog extends Vue {
+  visible = false;
+}
 </script>
---
+++ tests/unit/Dialog.spec.ts
@@ -0,0 +1,33 @@
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+import Dialog from '@/components/templates/Dialog.vue';
+import ElementUI, { Button } from 'element-ui';
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+
+describe('Dialog.vue', () => {
+  it('ダイアログを起動し、保存ボタンがあること', () => {
+    const wrapper = mount(Dialog, {
+      localVue,
+    });
+    wrapper.setData({ visible: true });
+    wrapper.vm.$nextTick();
+
+    //ダイアログがbodyに追加されるのでbodyからDOMを取得　
+    const elDialog = document.getElementsByClassName('el-dialog__wrapper')[0];
+    const buttons = elDialog.getElementsByClassName('el-button');
+
+    let saveButton = null;
+    const len = buttons.length;
+
+    for (let i = 0; i < len; i++) {
+      const button = buttons[i];
+      if (button && button.textContent === "保存") {
+        saveButton = button;
+      }
+    }
+
+    expect(saveButton).toBeTruthy;
+  })
+})
